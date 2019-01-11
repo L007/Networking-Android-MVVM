4209eaff629aaaa581d34c63d3ddf149659cc7f8
L007 <antoeldi@gmail.com>
2019-01-11 17:32:04 +0700

    add binding adapter

--- app/src/main/java/id/onestep/networkingandroidmvvm/views/MainActivity.kt
+++ app/src/main/java/id/onestep/networkingandroidmvvm/views/MainActivity.kt
@@ -4,13 +4,16 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import id.onestep.networkingandroidmvvm.R
+import id.onestep.networkingandroidmvvm.adapters.ListMovieAdapter
 import id.onestep.networkingandroidmvvm.databinding.ActivityMainBinding
 import id.onestep.networkingandroidmvvm.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private lateinit var viewModel : MainViewModel
+    private lateinit var adapter : ListMovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,17 @@ class MainActivity : AppCompatActivity() {
         binding.main=viewModel
 
         binding.btnSearch.setOnClickListener { viewModel.getMovie(binding.etCategory.getText().toString()) }
+
+    }
+
+    private fun setupRecyvleView(){
+        val layoutManager = LinearLayoutManager(this)
+        //binding. ke rvMovie
+        binding.rvMovie.layoutManager = layoutManager
+        adapter = ListMovieAdapter(this)
+
+        binding.rvMovie.adapter=adapter
+
 
     }
 }
